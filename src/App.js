13f0761effc435a22e8ed0217d15402353e20551
ira strikun <ira.strikun@gmail.com>
2018-09-24 16:57:21 +0300

    fixed to deploy ghpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  HashRouter
+} from "react-router-dom";
 import Homepage from "./Page";
 import About from "./About";
 
@@ -7,12 +12,14 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Router>
-          <Switch>
+        <HashRouter>
+          <div>
+            {/* <Switch> */}
             <Route exact path="/" component={Homepage} />
             <Route path="/about" component={About} />
-          </Switch>
-        </Router>
+            {/* </Switch> */}
+          </div>
+        </HashRouter>
       </React.Fragment>
     );
   }
