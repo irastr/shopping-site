039e1654668f9a7603885607db6324912a1e2aea
ira strikun <ira.strikun@gmail.com>
2018-09-24 18:08:19 +0300

    adaptive fixed

--- src/Testimonials/Testimonials.jsx
+++ src/Testimonials/Testimonials.jsx
@@ -5,25 +5,6 @@ import testOne from "../assets/img/testimonials-1.png";
 import testTwo from "../assets/img/testimonials-2.png";
 import testThree from "../assets/img/testimonials-3.png";
 
-// import { Carousel } from "react-responsive-carousel";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-// const Testimonials = () => {
-//   return (
-//     <div className="section-testimonials">
-//       <p className="testimonials__content">
-//         {" "}
-//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-//         reprehenderit numquam quos repellat est accusamus itaque suscipit. Saepe
-//         atque tenetur voluptate sequi. Voluptatum recusandae veniam quisquam
-//         eligendi, repellat fuga tenetur.
-//       </p>
-//     </div>
-//   );
-// };
-
-// export default Testimonials;
-
 class Testimonials extends Component {
   render() {
     var settings = {
